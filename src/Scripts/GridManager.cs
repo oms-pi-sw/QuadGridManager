43ade775b5a75089cb71c7dab3bffd9c699df418
NickF93 <niccolo.ferrari@hotmail.it>
2015-03-25 12:40:40 +0100

    QuadGridManager 0.1a_Alpha

---
+++ src/Scripts/GridManager.cs
@@ -0,0 +1,257 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public enum SlopeDetectionMode {
+	Euclidean, Vertices, Triangles, Face
+}
+
+public class GridManager : MonoBehaviour {
+	private static List<GridManager> instances = null;
+	private int actualInstanceIndex = -1;
+
+	public GameObject QuadTile;
+
+	public GridManagerCore[] cores;
+
+	public bool debug = false;
+
+	void Awake () {
+		if (instances == null)
+			instances = new List<GridManager>();
+		instances.Add (this);
+		actualInstanceIndex = instances.IndexOf (this);
+	}
+
+	// Use this for initialization
+	void Start () {
+		foreach (GridManagerCore core in cores)
+			core.Init (this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public GridManager GetActualInstance {
+		get {
+			return GridManager.instances[this.actualInstanceIndex];
+		}
+	}
+	public static GridManager[] GetInstances {
+		get {
+			return GridManager.instances.ToArray ();
+		}
+	}
+	public int GetActualInstanceIndex {
+		get {
+			return this.actualInstanceIndex;
+		}
+	}
+}
+
+
+[System.Serializable()]
+public class GridManagerCore {
+
+	private static List<GridManagerCore> instances = null;
+	private int actualInstanceIndex = -1;
+	private GridManager manager = null;
+
+	private GameObject tile;
+
+	public string name = "Grid";
+
+	public MapInfo infos;
+
+	public float offsetY = 0.25f;
+
+	public LayerMask mask;
+
+	public float maxSlope = 90f;
+	public SlopeDetectionMode slopeDetectionMode;
+	public bool mediumSlope = true;
+
+	private Vector3 tileDim;
+	private Vector3 mapDim;
+
+	private GameObject[][] TileMatrix;
+
+	private GameObject selectedTile = null;
+
+	public void Init (GridManager manager) {
+		this.manager = manager;
+		tile = this.manager.QuadTile;
+
+		if (instances == null)
+			instances = new List<GridManagerCore>();
+		instances.Add (this);
+		actualInstanceIndex = instances.IndexOf (this);
+
+		infos.SetMap ();
+
+		SetSize ();
+		CreateGrid ();
+	}
+
+	private void SetSize () {
+		tileDim = tile.GetComponent<Renderer>().bounds.size;
+		if (infos.UseTerrain)
+			mapDim = infos.GetActiveMap.GetComponent<Terrain>().terrainData.size;
+		else
+			mapDim = infos.GetActiveMap.GetComponent<Renderer>().bounds.size;
+	}
+
+	private Sides GetSides () {
+		return new Sides ((int)(mapDim.x / tileDim.x), (int)(mapDim.z / tileDim.z));
+	}
+
+	private Vector2 CalcGridSize () {
+		int nrOfSidesX = GetSides ().X;
+		int nrOfSidesZ = GetSides ().Z;
+		return new Vector2 (nrOfSidesX, nrOfSidesZ);
+	}
+
+	private Vector3 GetInitPos () {
+		Vector2 xzOffsets = new Vector2 ((mapDim.x - GetSides ().X * tileDim.x) / 2, (mapDim.z - GetSides ().Z * tileDim.z) / 2);
+		if (infos.UseTerrain)
+			return new Vector3 (tileDim.x / 2 + infos.GetActiveMap.transform.position.x + xzOffsets.x, infos.GetActiveMap.transform.position.y, mapDim.z - tileDim.z / 2 + infos.GetActiveMap.transform.position.z - xzOffsets.y);
+		else
+			return new Vector3 (-mapDim.x / 2 + tileDim.x / 2 + infos.GetActiveMap.GetComponent<Renderer>().bounds.center.x + xzOffsets.x, infos.GetActiveMap.GetComponent<Renderer>().bounds.center.y, mapDim.z/ 2 - tileDim.z / 2 + infos.GetActiveMap.GetComponent<Renderer>().bounds.center.z - xzOffsets.y);
+	}
+
+	public Vector3 GetWorldCoord (Vector2 tilePos) {
+		Vector3 initPos = GetInitPos ();
+		float x = initPos.x + tilePos.x * tileDim.x;
+		float z = initPos.z - tilePos.y * tileDim.z;
+		return new Vector3(x, offsetY, z);
+	}
+
+	private void CreateGrid () {
+		Vector2 gridSize = CalcGridSize ();
+		GameObject quadGridGO = new GameObject("QuadGrid");
+		quadGridGO.transform.parent = GetManager.transform;
+		TileMatrix = new GameObject[(int)gridSize.x][];
+		for (int i = 0; i < TileMatrix.Length; i++)
+			TileMatrix[i] = new GameObject[(int)gridSize.y];
+
+		for (int x = 0; x < (int)gridSize.x; x++) {
+			for (int z = 0; z < (int)gridSize.y; z++) {
+				GameObject tile = (GameObject)GameObject.Instantiate (this.tile);
+				Vector2 tilePos = new Vector2(x, z);
+				tile.transform.position = GetWorldCoord (tilePos);
+				tile.transform.parent = quadGridGO.transform.parent;
+				TileManager tm = tile.GetComponent<TileManager> ();
+				tm.ManagerIndex = actualInstanceIndex;
+				tm.Core = this;
+				tm.Manager = manager;
+				TileMatrix[x][z] = tile;
+			}
+		}
+	}
+
+	public GameObject SelectedTile {
+		get {
+			return selectedTile;
+		}
+		set {
+			selectedTile = value;
+		}
+	}
+
+	public GridManager GetManager {
+		get {
+			return this.manager;
+		}
+	}
+
+	public float GetMaxHeight {
+		get {
+			if (infos.UseTerrain)
+				return infos.GetActiveMap.transform.TransformPoint (infos.GetActiveMap.GetComponent<Terrain>().terrainData.size).y;
+			else
+				return infos.GetActiveMap.transform.TransformPoint (infos.GetActiveMap.GetComponent<Renderer>().bounds.size).y;
+		}
+	}
+
+	public int GetActualInstanceIndex {
+		get {
+			return this.actualInstanceIndex;
+		}
+	}
+
+	public static GridManagerCore[] GetInstances {
+		get {
+			return GridManagerCore.instances.ToArray ();
+		}
+	}
+
+	public static GridManagerCore GetInstance (int index) {
+		return GridManagerCore.instances[index];
+	}
+
+	public GridManagerCore GetActualInstance {
+		get {
+			return GridManagerCore.instances[this.actualInstanceIndex];
+		}
+	}
+
+	public static GridManager[] GetGridManagerInstances {
+		get {
+			return GridManager.GetInstances;
+		}
+	}
+	public LayerMask GetLayerMask {
+		get {
+			return this.mask;
+		}
+	}
+}
+
+[System.Serializable()]
+public class MapInfo {
+	public GameObject MapMesh;
+	public GameObject MapTerrain;
+	public bool useTerrain;
+	private GameObject activeMap = null;
+
+	public void SetMap () {
+		if (MapMesh == null && MapTerrain != null) {
+			activeMap = MapTerrain;
+			useTerrain = true;
+		} else if (MapMesh != null && MapTerrain == null) {
+			activeMap = MapMesh;
+			useTerrain = false;
+		} else if (MapMesh != null && MapTerrain != null) {
+			if (useTerrain) {
+				activeMap = MapTerrain;
+			} else
+				activeMap = MapMesh;
+		} else
+			activeMap = null;
+	}
+
+	public GameObject GetActiveMap {
+		get {
+			return activeMap;
+		}
+	}
+
+	public bool UseTerrain {
+		get {
+			return useTerrain;
+		}
+	}
+}
+
+public class Sides {
+	private int x, z;
+	public Sides (int x, int z) {
+		this.x = x;
+		this.z = z;
+	}
+	public int X { get { return this.x; } }
+	public int Z { get { return this.z; } }
+}
---
+++ src/Scripts/TileManager.cs
@@ -0,0 +1,328 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileManager : MonoBehaviour {
+
+	private int managerIndex = -1;
+	private GridManagerCore core;
+	private GridManager manager;
+
+	public GameObject TempTower;
+
+	public Material normalMaterial, mouseOverMaterial, selectedTileMaterial;
+
+	private bool selected = false;
+
+	private bool actived = true;
+
+	// Use this for initialization
+	void Start () {
+		DetectSlope ();
+		SetMesh ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public bool Active {
+		get {
+			return this.actived;
+		}
+	}
+
+	public Vector3 GetMeshCenterInWorldPosition {
+		get {
+			return transform.TransformPoint (GetComponent<MeshFilter> ().mesh.bounds.center);
+		}
+	}
+
+	public int ManagerIndex {
+		get {
+			return managerIndex;
+		}
+		set {
+			if (managerIndex == -1)
+				managerIndex = value;
+		}
+	}
+
+	public GridManagerCore Core {
+		get {
+			return core;
+		}
+		set {
+			if (core == null)
+				core = value;
+		}
+	}
+
+	public GridManager Manager {
+		get {
+			return manager;
+		}
+		set {
+			if (manager == null)
+				manager = value;
+		}
+	}
+
+	private void DetectSlope () {
+		switch (core.slopeDetectionMode) {
+		case SlopeDetectionMode.Face:
+			if (GetPlaneSlope () * Mathf.Rad2Deg > core.maxSlope)
+				Deactivate ();
+			break;
+		case SlopeDetectionMode.Triangles:
+			if (GetTrianglesMaxSlope () * Mathf.Rad2Deg > core.maxSlope)
+				Deactivate ();
+			break;
+		case SlopeDetectionMode.Vertices:
+			if (GetVerticesMaxSlope () * Mathf.Rad2Deg > core.maxSlope)
+				Deactivate ();
+			break;
+		default:
+			if (GetEuclideanMaxSlope () * Mathf.Rad2Deg > core.maxSlope)
+				Deactivate ();
+			break;
+		}
+	}
+
+	private void Deactivate () {
+		this.GetComponent<Renderer>().material.color = new Color (0, 0, 0, 0);
+		this.actived = false;
+	}
+
+	private float GetPlaneSlope () {
+		Mesh mesh = GetComponent<MeshFilter> ().mesh;
+		Vector3 center = mesh.bounds.center;
+		center = transform.TransformPoint (center);
+
+		float height = core.GetMaxHeight + 1;
+
+		center.y = height;
+
+		Vector3 normal = Vector3.zero;
+
+		RaycastHit hit;
+
+		if (GetRaycastDown (center, out hit))
+		    normal = hit.normal;
+
+		normal.Normalize ();
+
+		float slope = Mathf.Acos (Mathf.Abs (Vector3.Dot (Vector3.up, normal)));
+
+		if (manager.debug)
+			print (slope * Mathf.Rad2Deg);
+
+		return slope;
+	}
+
+	private float GetVerticesMaxSlope () {
+		Mesh mesh = GetComponent<MeshFilter> ().mesh;
+		Vector3[] origins = mesh.vertices;
+
+		float height = core.GetMaxHeight + 1;
+
+		RaycastHit hit;
+
+		float slope = 0;
+		if (!core.mediumSlope)
+			slope = -1;
+
+		for (int i = 0; i < origins.Length; i++) {
+
+			origins[i] = transform.TransformPoint (origins[i]);
+			origins[i].y = height;
+
+			if (GetRaycastDown (origins[i], out hit)) {
+				Vector3 normal = hit.normal.normalized;
+				float s = Mathf.Acos (Mathf.Abs (Vector3.Dot (Vector3.up, normal)));
+
+				if (core.mediumSlope)
+					slope += s;
+				else
+					if (s > slope)
+						slope = s;
+			}
+		}
+		if (core.mediumSlope)
+			slope /= origins.Length;
+
+		if (manager.debug)
+			print (slope * Mathf.Rad2Deg);
+
+		return slope;
+	}
+
+	private float GetTrianglesMaxSlope () {
+		Mesh mesh = GetComponent<MeshFilter> ().mesh;
+		int[] triangles = mesh.triangles;
+		Vector3[] origins = mesh.vertices;
+
+		float height = core.GetMaxHeight + 1;
+		RaycastHit hit;
+
+		float slope = 0;
+
+		if (!core.mediumSlope)
+			slope = -1;
+
+		for (int i = 0; i < triangles.Length; i++) {
+			Vector3 normal = Vector3.zero;
+
+			for (int j = 0; j < 3; j++) {
+				Vector3 tVect = transform.TransformPoint (origins[triangles[i++]]);
+				if (GetRaycastDown (new Vector3(tVect.x, height, tVect.z), out hit))
+					normal += hit.normal;
+			}
+			--i;
+			normal = (normal / 3).normalized;
+			float s = Mathf.Acos (Mathf.Abs (Vector3.Dot (Vector3.up, normal)));
+
+			if (core.mediumSlope)
+				slope += s;
+			else
+				if (s > slope)
+					slope = s;
+		}
+
+		if (core.mediumSlope)
+			slope /= (triangles.Length / 3);
+
+		if (manager.debug)
+			print (slope * Mathf.Rad2Deg);
+		
+		return slope;
+	}
+
+	private float GetEuclideanMaxSlope () {
+		Mesh mesh = GetComponent<MeshFilter> ().mesh;
+		int[] triangles = mesh.triangles;
+		Vector3[] origins = mesh.vertices;
+
+		float height = core.GetMaxHeight + 1;
+		RaycastHit hit;
+		
+		float slope;
+		if (core.mediumSlope)
+			slope = 0;
+		else
+			slope = -1;
+		
+		for (int i = 0; i < triangles.Length; i++) {
+			Vector3 normal = Vector3.zero;
+
+			Vector3 vTemp = transform.TransformPoint (origins[triangles[i++]]);
+			Vector3 v1 = new Vector3(vTemp.x, height, vTemp.z);
+
+			vTemp = transform.TransformPoint (origins[triangles[i++]]);
+			Vector3 v2 = new Vector3(vTemp.x, height, vTemp.z);
+
+			vTemp = transform.TransformPoint (origins[triangles[i]]);
+			Vector3 v3 = new Vector3(vTemp.x, height, vTemp.z);
+
+			if (GetRaycastDown (v1, out hit))
+				v1 = hit.point;
+			if (GetRaycastDown (v2, out hit))
+				v2 = hit.point;
+
+			if (GetRaycastDown (v3, out hit))
+				v3 = hit.point;
+
+			v1 = TransformCanonicalAxes (v1);
+			v2 = TransformCanonicalAxes (v2);
+			v3 = TransformCanonicalAxes (v3);
+			normal = new Vector3 (detM2 (v2.y - v1.y, v2.z - v1.z, v3.y - v1.y, v3.z - v1.z), -detM2 (v2.x - v1.x, v2.z - v1.z, v3.x - v1.x, v3.z - v1.z), detM2 (v2.x - v1.x, v2.y - v1.y, v3.x - v1.x, v3.y - v1.y));
+			normal.Normalize ();
+			normal = TransformUnityAxes (normal);
+			float s = Mathf.Acos (Mathf.Abs (Vector3.Dot (Vector3.up, normal)));
+
+			if (core.mediumSlope)
+				slope += s;
+			else 
+				if (s > slope)
+					slope = s;
+		}
+
+		if (core.mediumSlope)
+			slope /= (triangles.Length / 3);
+
+		if (manager.debug)
+			print (slope * Mathf.Rad2Deg);
+		return slope;
+	}
+
+	private Vector3 TransformCanonicalAxes (Vector3 v) {
+		return new Vector3 (v.x, v.z, -v.y);
+	}
+
+	private Vector3 TransformUnityAxes (Vector3 v) {
+		return new Vector3 (v.x, -v.z, v.y);
+	}
+
+	/* 
+	 * 		MATRIX
+	 * 		[a b]
+	 * 		[c d]
+	 */
+	private float detM2 (float a, float b, float c, float d) {
+		return (a * d - b * c);
+	}
+
+	void OnMouseOver () {		
+		if (Input.GetMouseButtonUp(0)) {
+			GetComponent<Renderer>().material = selectedTileMaterial;
+			selected = true;
+			core.SelectedTile = gameObject;
+			GameObject tower = GameObject.Instantiate (TempTower) as GameObject;
+			tower.transform.position = GetMeshCenterInWorldPosition + new Vector3 (0, tower.transform.TransformPoint (tower.GetComponent<MeshFilter> ().mesh.bounds.center).y - tower.transform.TransformPoint (tower.GetComponent<MeshFilter> ().mesh.bounds.min).y, 0);
+		} else if (!selected) {
+			GetComponent<Renderer>().material = mouseOverMaterial;
+		}
+	}
+
+	void OnMouseEnter () {
+		GetComponent<Renderer>().material = mouseOverMaterial;
+		selected = false;
+		core.SelectedTile = null;
+	}
+
+	void OnMouseExit () {
+		GetComponent<Renderer>().material = normalMaterial;
+		selected = false;
+		core.SelectedTile = null;
+	}
+
+	private void SetMesh () {
+		Mesh mesh = GetComponent<MeshFilter> ().mesh;
+		var vertices = new Vector3[mesh.vertices.Length];
+		for (int i = 0; i < mesh.vertices.Length; i++) {
+			Vector3 wPoint = transform.TransformPoint (mesh.vertices[i]);
+			float yHeight = Core.GetMaxHeight + 1;
+			RaycastHit hit;
+			Vector3 calcVert = Vector3.zero;
+			if (GetRaycastDown (new Vector3 (wPoint.x, yHeight, wPoint.z), out hit)) {
+				calcVert = hit.point;
+			}
+			calcVert.y += Core.offsetY;
+			calcVert.x = wPoint.x;
+			calcVert.z = wPoint.z;
+			vertices[i] = transform.InverseTransformPoint (calcVert);
+		}
+		mesh.vertices = vertices;
+		mesh.RecalculateBounds ();
+		mesh.RecalculateNormals ();
+		Destroy (GetComponent<MeshCollider> ());
+		if (this.actived)
+			gameObject.AddComponent<MeshCollider> ();
+	}
+	private bool GetRaycastDown (Vector3 point, out RaycastHit hit) {
+		if (Manager.debug)
+			Debug.DrawRay (point, Vector3.down, Color.red, Mathf.Infinity);
+		return Physics.Raycast (point, Vector3.down, out hit, Mathf.Infinity, Core.GetLayerMask);
+	}
+
+}
